7d804db77eeca0ade250a4b30fb33b4bd0552ba1
Selva Chezhian <selvachezhian@kreatio.com>
2014-01-19 16:25:24 +0530

    schema file gen

--- db/schema.rb
+++ db/schema.rb
@@ -24,11 +24,7 @@ ActiveRecord::Schema.define(:version => 20140111104305) do
     t.date "date_of_birth"
     t.integer "password_id"
     t.integer "address_id"
-    t.integer "city_id"
-    t.integer "state_id"
-    t.integer "country_id"
-    t.string "post_code"
-    t.string "mobile_no"
+    t.integer "contact_detail_id"
     t.string "subscriber_type_id"
     t.integer "free_subscriber_id"
     t.integer "user_name_id"
@@ -86,6 +82,35 @@ ActiveRecord::Schema.define(:version => 20140111104305) do
     t.datetime "updated_at"
   end
 
+  create_table "contact_details", :force => true do |t|
+    t.boolean "subscriber_contact"
+    t.boolean "primary_contact"
+    t.boolean "work_contact"
+    t.boolean "area_contact"
+    t.boolean "agent_contact"
+    t.string "contact_status"
+    t.string "contact_type"
+    t.string "primary_email"
+    t.string "alternate_email"
+    t.string "primary_mobileno"
+    t.string "primary_telephone"
+    t.string "alternate_nubmer"
+    t.string "fax"
+    t.string "work_mobile"
+    t.string "work_telephone"
+    t.string "work_email"
+    t.string "work_location"
+    t.string "work_type"
+    t.string "work_fax"
+    t.text "desigination"
+    t.string "work_website"
+    t.integer "address_id"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "address", :force => true do |t|
     t.string "address_1"
     t.string "address_2"
@@ -110,20 +135,11 @@ ActiveRecord::Schema.define(:version => 20140111104305) do
 
   create_table "agents", :force => true do |t|
     t.string "name"
-    t.integer "address_id"
-    t.integer "campaign_ids"
-    t.datetime 'created_at'
-    t.datetime "updated_at"
-  end
-
-  create_table "campaigns", :force => true do |t|
-    t.string "name"
     t.string "alter_name"
-    t.string "description"
-    t.integer "campaign_type_id"
+    t.text "description"
+    t.integer "address_id"
+    t.string "area_ids"
     t.integer "main_office_id"
-    t.integer "agent_id"
-    t.integer "campaign_office_id"
     t.datetime 'created_at'
     t.datetime "updated_at"
   end
@@ -131,35 +147,88 @@ ActiveRecord::Schema.define(:version => 20140111104305) do
   create_table "areas", :force => true do |t|
     t.string "name"
     t.string "user_name"
+    t.datetime "display_name"
     t.integer "password_id"
     t.string "email_id"
     t.string "alternate_name"
+    t.integer "contact_detail_id"
     t.string "telephone"
     t.string "fax"
     t.string "website"
-    t.string "description"
-    t.integer "campaign_id"
-    t.integer "campaign_office_id"
+    t.text "description"
+    t.string "overview"
+    t.integer "agent_id"
+    t.integer "area_office_id"
+    t.integer "main_office_id"
+    t.integer "area_review_id"
+    t.integer "contact_detail_id"
+    t.integer "area_event_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "campaign_offices", :force => true do |t|
+  create_table "area_other_deatails", :force => true do |t|
+    t.datetime "open_time"
+    t.datetime "close_time"
+    t.string "no_of_products"
+    t.integer "no_of_staffs"
+    t.datetime "area_stated_time"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "area_subscribers", :force => true do |t|
+    t.integer "area_id"
+    t.integer "area_review_id"
+    t.string "no_subscribers"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "area_events", :force => true do |t|
+    t.string "name"
+    t.string "dispaly_name"
+    t.datetime "publish_date"
+    t.datetime "expiry_date"
+    t.boolean "active"
+    t.boolean "expired"
+  end
+
+  create_table "area_offices", :force => true do |t|
     t.integer "address_id"
     t.boolean "main_office"
   end
 
-  create_table "campaign_types", :force => true do |t|
-    t.string "name"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
   create_table "products", :force => true do |t|
     t.string "name"
+    t.string "alternate_name"
+    t.string "title"
+    t.string "key_word"
+    t.text "description"
+    t.integer "image_id"
+    t.integer "product_status_id"
+    t.integer "product_detail_id"
+    t.boolean "active"
+    t.boolean "expired"
+    t.boolean "area_event"
+    t.integer "group_id"
     t.integer "product_type_id"
+    t.integer "agent_id"
+    t.integer "area_id"
+    t.integer "area_event_id"
+    t.datetime "publish_date"
+    t.datetime "expiry_date"
+    t.string "tag"
     t.datetime "created_at"
     t.datetime "update_at"
+  end
+
+  create_table "product_deatils", :force => true do |t|
+    t.integer "product_id"
+    t.integer "product_type_id"
+    t.integer "video"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "product_types", :force => true do |t|
@@ -168,10 +237,130 @@ ActiveRecord::Schema.define(:version => 20140111104305) do
     t.datetime "update_at"
   end
 
+  create_table "product_status", :force => true do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "area_reviews", :force =>  true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "groups", :force => true do |t|
     t.string "name"
     t.datetime "created_at"
     t.datetime "update_at"
+  end
+
+  create_table "articles", :force => true do |t|
+    t.string "title"
+    t.string "sub_title"
+    t.text "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "video_details", :force => true do |t|
+    t.string "name"
+    t.string "title"
+    t.string "display_name"
+    t.text "description"
+    t.string "video_path"
+    t.string 'video_format'
+    t.string "author"
+    t.string "force_code"
+    t.string "file_size"
+    t.string "file_name"
+    t.integer "width"
+    t.integer "height"
+    t.string "resolution"
+    t.integer "sequence_number"
+    t.boolean "licence"
+    t.string "licence_type"
+    t.string "licence_key"
+    t.boolean "avatar"
+    t.integer "avatar_id"
+    t.integer "image_id"
+    t.integer "status_id"
+    t.string "caption"
+    t.datetime "created_by"
+    t.datetime "updated_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "images", :force => true do |t|
+    t.string "name"
+    t.string "title"
+    t.text "description"
+    t.string "author"
+    t.string "atl_tag"
+    t.boolean "licence"
+    t.string "licence_type"
+    t.string "licence_key"
+    t.boolean "avatar"
+    t.integer "avatar_id"
+    t.string "image_type"
+    t.integer "sequence_number"
+    t.integer "width"
+    t.integer "height"
+    t.string "resolution"
+    t.string "file_size"
+    t.integer "gallery_id"
+    t.string "image_path"
+    t.string "image_source"
+    t.integer "status_id"
+    t.string "caption"
+    t.integer "created_by"
+    t.integer "updated_by"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "avatars", :force => true do |t|
+    t.string "name"
+    t.string "type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "audios", :force => true do |t|
+    t.string "name"
+    t.string "title"
+    t.string "author"
+    t.boolean "licence"
+    t.string "licence_type"
+    t.string "licence_key"
+    t.text "description"
+    t.string "audio_path"
+    t.string "audio_type"
+    t.integer "status_id"
+    t.integer "image_id"
+    t.integer "file_size"
+    t.string "file_name"
+    t.string "caption"
+    t.boolean "avatar"
+    t.integer "avatar_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "medias", :force => true do |t|
+    t.integer "video_detail_id"
+    t.integer "image_id"
+    t.integer "product_id"
+    t.integer "area_id"
+    t.integer "agent_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "status", :force => true do |t|
+    t.string "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "social_hubs", :force => true do |t|
